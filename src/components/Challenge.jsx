1ed71feff3ddfaad051f328cfe9a008434c2fb7f
alexandraru <115844615+alexandraru@users.noreply.github.com>
2024-02-04 01:40:23 +0200

    8com

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -13,22 +13,22 @@ function Challenge (props){
             </div>
             <div className="video-box">
                 <h2 className="video-box-title">День 2</h2>
-                <ReactPlayer className="video-box-video" url={"https://youtu.be/MHu7UEvOJfU?si=n6utv4YZAZ_27V_c"} controls/>
+                <ReactPlayer className="video-box-video" style={{border: "1px solid rgb(11, 11, 112)"}} url={"https://youtu.be/MHu7UEvOJfU?si=n6utv4YZAZ_27V_c"} controls/>
             </div>
             <div className="video-box">
                 <h2 className="video-box-title">День 3</h2>
                 <p className="video-box-text">{el.challenge2}</p>
-                <ReactPlayer className="video-box-video" url={"https://youtu.be/JuTXFzHK1bc?si=bMZdcVyhRqPZLPLN"} controls/>
-                <ReactPlayer className="video-box-video" url={"https://youtu.be/awEUHXCkI9M?si=DGqZmP_0zuVBFJrM"} controls/>
+                <ReactPlayer className="video-box-video" style={{border: "1px solid rgb(11, 11, 112)"}} url={"https://youtu.be/JuTXFzHK1bc?si=bMZdcVyhRqPZLPLN"} controls/>
+                <ReactPlayer className="video-box-video" style={{border: "1px solid rgb(11, 11, 112)"}} url={"https://youtu.be/awEUHXCkI9M?si=DGqZmP_0zuVBFJrM"} controls/>
             </div>
             <div className="video-box">
                 <h2 className="video-box-title">День 4</h2>
-                <ReactPlayer className="video-box-video" url={"https://youtu.be/O6uBOb1NQvc?si=p1NojeBSHn8BXnb0"} controls/>
+                <ReactPlayer className="video-box-video" style={{border: "1px solid rgb(11, 11, 112)"}} url={"https://youtu.be/O6uBOb1NQvc?si=p1NojeBSHn8BXnb0"} controls/>
             </div>
             <div className="video-box">
                 <h2 className="video-box-title">День 5</h2>
                 <p className="video-box-text">{el.challenge3}</p>
-                <ReactPlayer className="video-box-video" url={"https://youtu.be/hI3g19hmjqA?si=K_FWgSHbCe3i3hh1"} controls/>
+                <ReactPlayer className="video-box-video" style={{border: "1px solid rgb(11, 11, 112)"}} url={"https://youtu.be/hI3g19hmjqA?si=K_FWgSHbCe3i3hh1"} controls/>
             </div>
         
         </div> ))}
